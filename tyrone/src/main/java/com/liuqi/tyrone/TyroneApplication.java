298d6e8db41a55820b52258f30a7f1c0d76e15ae
tyrone <76234648@qq.com>
2018-06-04 11:59:31 +0800

    测试

--- tyrone/src/main/java/com/liuqi/tyrone/TyroneApplication.java
+++ tyrone/src/main/java/com/liuqi/tyrone/TyroneApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 2 zookeeper分布式锁
  * 3 mybatis乐观锁（插件整合）
  * 4 dubbo整合
- * 5 待定
+ * 5 待定1
  */
 @SpringBootApplication
 public class TyroneApplication {
