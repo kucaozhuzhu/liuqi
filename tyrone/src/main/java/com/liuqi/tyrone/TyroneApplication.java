102961977fa75789bc03534f893e348a8f7b9b95
tyrone <76234648@qq.com>
2018-06-04 11:14:10 +0800

    first

---
+++ tyrone/src/main/java/com/liuqi/tyrone/TyroneApplication.java
@@ -0,0 +1,19 @@
+package com.liuqi.tyrone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 1 分布式任务调度
+ * 2 zookeeper分布式锁
+ * 3 mybatis乐观锁（插件整合）
+ * 4 dubbo整合
+ * 5
+ */
+@SpringBootApplication
+public class TyroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyroneApplication.class, args);
+	}
+}
---
+++ tyrone/src/test/java/com/liuqi/tyrone/TyroneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liuqi.tyrone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TyroneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
